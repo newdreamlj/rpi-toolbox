fb2f79e658442fa6c0dcf7882f4a33245ba873d8
Liu Jun <liujun.richard@gmail.com>
2017-08-30 01:45:14 +0800

    UART works

--- peripheral/uart/co2.py
+++ peripheral/uart/co2.py
@@ -2,37 +2,45 @@ import RPi.GPIO as GPIO
 import time
 import serial
 
+def co2_init():
+    PIN_RESET = 12;
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+    # func = GPIO.gpio_function(PIN_RESET);
+    # print func
+    #if func != GPIO.OUT:
+    GPIO.setup(PIN_RESET,GPIO.OUT)
+    GPIO.output(PIN_RESET,GPIO.HIGH)
+    ser = serial.Serial(
+	port='/dev/serial0',
+	baudrate = 9600,
+	parity=serial.PARITY_NONE,
+	stopbits=serial.STOPBITS_ONE,
+	bytesize=serial.EIGHTBITS,
+	timeout=1)
+    return ser
 
-def serial_init():
-	global ser
-	PIN_RESET = 12;
-	GPIO.setmode(GPIO.BOARD)	
-	GPIO.setwarnings(False)
-	# func = GPIO.gpio_function(PIN_RESET);
-	# print func
- 	#if func != GPIO.OUT:
-	GPIO.setup(PIN_RESET,GPIO.OUT)
-	GPIO.output(PIN_RESET,GPIO.HIGH)
-	ser = serial.Serial(
-		port='/dev/serial1',
-		baudrate = 9600,
-		parity=serial.PARITY_NONE,
-		stopbits=serial.STOPBITS_ONE,
-		bytesize=serial.EIGHTBITS,
-		timeout=1)
-
-def serial_read():
-	global ser
-	try:                                       # stops program failing if no serial data
-		response = ser.read(1)  # read 3 lines of serial port data
-		print "%x" % response[0]
-		ser.close()
-	except Exception, e:
-		print e
-		ser.close()	
+def co2_read(ser):
+    try:                        # stops program failing if no serial data
+        cmd = bytearray([0x42,0x4d,0xe3,0x00,0x00,0x01,0x72])
+        ser.write(cmd)
+        time.sleep(0.01)
+        response = ser.read(12)  # read 3 lines of serial port data
+	co2 = (ord(response[4])<<8) + ord(response[5])
+        # print "CO2 = %d" % co2
+        return co2
+    except Exception, e:
+	print e
+	ser.close()
+        return -1
 	# x=ser.readall()
 
 if __name__ == "__main__":
-	serial_init()
-	serial_read()
-
+    ser = co2_init()
+    time.sleep(10)
+    print "CO2 = %d" % co2_read(ser)
+    time.sleep(10)
+    print "CO2 = %d" % co2_read(ser)
+    time.sleep(10)
+    print "CO2 = %d" % co2_read(ser)
+    ser.close()
